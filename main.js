cba7cedb40bc1c0a20ef09a65e2e351270c56ab6
Audeo <audeo@umich.edu>
2020-07-27 17:52:15 -0400

    Added some blinky thing because why not

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ function mouseOutAction(btnId)
     console.log("mouseOutEvent on " + btnId);
     document.getElementById(btnId).style.backgroundColor = "#767676";
 }
+
+var pulser;
+let click = 0;
+function pulse(){
+    click++;
+    //console.log(click % 2);
+    if(click % 2 == 1) pulser = setInterval(pulseInternal, 1000);
+    else clearInterval(pulser);
+}
+function pulseInternal(){
+    
+    if (document.body.style.backgroundColor == "white")
+        document.body.style.backgroundColor = "red";
+    else document.body.style.backgroundColor = "white";
+}
